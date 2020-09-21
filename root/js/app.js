0ca81384c04322753651e2e560a2827f92d22626
Jenna Murphy <jmurphy@mcad.edu>
2020-09-20 22:31:21 -0500

    CSS updates

--- root/js/app.js
+++ root/js/app.js
@@ -1,66 +1,78 @@
 //var slider;
 //var pos;
- // var slider = document.getElementById("myRange").value;
- var slider = null;
+let slider
 
 function setup() {
-createCanvas(1500, 1200);
-   angleMode(DEGREES);
-   // slider = createSlider(0, 255, 120);
+ canvas = createCanvas(1000, 1000);
+
+
+  canvas.parent('main');
+
+  p1 = createP('Slider 1');
+  p1.parent('menu');
+  slider = createSlider(0, 255, 120).class('slider');
    // slider.position(20,130);
-   // slider2 = createSlider(0, 255, 120);
+  slider.parent('menu');
+
+  p2 = createP('Slider 2');
+  p2.parent('menu');
+  slider2 = createSlider(0, 255, 120).class('slider');
    // slider2.position(20,160);
-   // slider3 = createSlider(0, 255, 120);
+  slider2.parent('menu');
+
+  p3 = createP('Slider 3');
+  p3.parent('menu');
+  slider3 = createSlider(0, 255, 120).class('slider');
    // slider3.position(20,190);
-   // slider4 = createSlider(0, 255, 120);
+  slider3.parent('menu');
+
+  p4 = createP('Slider 4');
+  p4.parent('menu');
+  slider4 = createSlider(0, 255, 120).class('slider');
    // slider4.position(20,220);
-   // slider5 = createSlider(0, 1, 1);
+  slider4.parent('menu');
+
+  p5 = createP('Slider 5');
+  p5.parent('menu');
+  slider5 = createSlider(0, 1, 1).class('slider');
    // slider5.position(20,250);
-   // slider6 = createSlider(0, 255, 120);
+  slider5.parent('menu');
+
+  p6 = createP('Slider 6');
+  p6.parent('menu');
+  slider6 = createSlider(0, 255, 120).class('slider');
    // slider6.position(20,280);
-   // button = createButton('save image');
-   // button.position(20, 300);
-   // button.mousePressed(downloadPNG);
+   slider6.parent('menu');
+
+   angleMode(DEGREES);
+
+   button = createButton('Save Image');
+   button.parent('menu');
+   // button.position(20, 400);
+   button.mousePressed(downloadPNG);
 //   pos = 0.0;
 //   inc = 3.0
 //   pos2 = 0.0;
   // inc2 = 7.0
-  noLoop();
 }
 
 function downloadPNG() {
   saveCanvas();
-
 }
 
 function draw() {
   clear();
-  // text('for loop control', slider.x + slider.width, 60);
-  // text('vertex shape1', slider2.x + slider2.width, 90);
-  // text('vertex shape2', slider3.x  + slider3.width, 120);
-  // text('vertex shape3', slider4.x  + slider4.width, 120);
-
-
+  background(254, 249, 243);
   translate(width/2, height/2);
 
-//  var mySinVal = sin(pos);
-//  var mySinVal2 = sin(pos2)
 
-//  amplified = mySinVal * 50;
-//  amplified2 = mySinVal2 *50;
-
-//  for (x = 0; x <= 360; x = x+ 10) {
-
-//    curve(0, 10, 50, amplified2, amplified, 10, 10, 10)
-//    rotate(x);
-
-for (x = 2; x <= slider.value()*3; x = x+ 3)
+for (x = 2; x <= slider4.value()*3; x = x+ 3)
   for (y = 0; y <= 600; y = y +10) {
     noFill();
     beginShape();
-    strokeWeight(slider.value()/3);
-    vertex(slider.value(), slider.value()*2);
-    quadraticVertex(x/6, y/PI, slider.value(), slider.value());
+    strokeWeight(slider5.value()/3);
+    vertex(slider.value(), slider2.value()*2);
+    quadraticVertex(x/6, y/PI, slider3.value(), slider6.value());
     endShape();
     beginShape();
     strokeWeight(.07);
